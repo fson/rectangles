306894106cc45286eed05bc020d00a0cb75d9860
Ville Immonen <ville.immonen@iki.fi>
2012-10-31 05:41:10 +0200

    Array.isArray

--- js/shim.js
+++ js/shim.js
@@ -33,3 +33,12 @@ if (!Function.prototype.bind) {
     return fBound;
   };
 }
+
+// Array.isArray
+// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/
+// Array/isArray
+if (!Array.isArray) {
+  Array.isArray = function (vArg) {
+    return Object.prototype.toString.call(vArg) === "[object Array]";
+  };
+}
